a8255264c489707487b2b8a651741f9412719552
xingxingge <447994518@qq.com>
2019-04-01 13:25:15 +0800

    -c

--- src/main/java/com/leetcode/P31_NextPermutation.java
+++ src/main/java/com/leetcode/P31_NextPermutation.java
@@ -2,28 +2,26 @@ package com.leetcode;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-
 public class P31_NextPermutation {
   public static void main(String[] args) {
     //    int[] intArray = new int[]{10,9,8,7,6};
     int[] intArray = new int[] { 1 };
     new P31_NextPermutation().nextPermutation(intArray);
-    Arrays.stream(intArray).forEach(System.out::println);
+//    Arrays.stream(intArray).forEach(System.out::println);
 
   }
 
   @Test public void test1() {
     int[] array = { 1, 3, 2 };
     nextPermutation(array);
-    Arrays.stream(array).forEach(System.out::println);
+//    Arrays.stream(array).forEach(System.out::println);
   }
 
   @Test public void test2() {
     //3,1,2,4
     int[] array = { 2, 4, 3, 1 };
     nextPermutation(array);
-    Arrays.stream(array).forEach(System.out::println);
+//    Arrays.stream(array).forEach(System.out::println);
   }
 
   /**
